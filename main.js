e2d86ec68d81688f7bf28bf80cc5695255a35b77
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2023-08-02 22:33:33 +0100

    more legible source code for main.js

--- main.js
+++ main.js
@@ -7,17 +7,23 @@ const [
   [12, false], [(12*12), true], "shadow-host"
 ];
 
-matrix_label:; 
-/* DEV_NOTE # UNISCALAR has to go hand in hand with UNIDIMENSIONAL, whilst MULTISCALAR hand in hand with MULTIDIMENSIONAL */
-[...new Array(/* UNISCALAR ||  */MULTISCALAR).fill(0)].forEach((_, j)=>{
+/* DEV_NOTE (IMPORTANT) # UNISCALAR has to go hand in hand with UNIDIMENSIONAL, whilst MULTISCALAR hand in hand with MULTIDIMENSIONAL in array_templateN and dimConfig mutually exclusively */
+array_template1:
+[...new Array(/* UNISCALAR ||  */ MULTISCALAR).fill(0)].forEach((_, j)=>{
 
-  new Row({id: `${Row.namespace}${1+j}`, shadowHost: `#${SHADOWHOST}`, dimConfig: {scalar: MULTISCALAR/*  || MULTISCALAR */, dimension: /* UNIDIMENSIONAL ||  */MULTIDIMENSIONAL}}) && [...new Array(/* UNISCALAR || */ MULTISCALAR).fill(0)].forEach((_, k)=>{
-    const eachRow = document.getElementById(SHADOWHOST).children;
-    /* DEV_NOTE # shadow root property is not mandatory, although it indicates open shadow dom */
-    eachRow[j].shadowRoot.appendChild(
-        new Col({id: `${Row.namespace}${1+k}:${Col.namespace}${1+j}`})
+    array_template2:
+    [...new Array(/* UNISCALAR || */ MULTISCALAR)
+    .fill(
+          new Row({id: `${Row.namespace}${1+j}`, shadowHost: `#${SHADOWHOST}`, dimConfig: {scalar: MULTISCALAR /*  || MULTISCALAR */, dimension: /* UNIDIMENSIONAL ||  */ MULTIDIMENSIONAL}})
       )
-  });
+    ]
+    .forEach((_, k)=>{
+      const eachRow = document.getElementById(SHADOWHOST).children;
+      /* DEV_NOTE # shadow root property is not mandatory, although it indicates open shadow dom */
+      eachRow[j].shadowRoot.appendChild(
+          new Col({id: `${Row.namespace}${1+k}:${Col.namespace}${1+j}`})
+        )
+    });
 
 })
 
