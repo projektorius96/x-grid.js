2b266bb4eb4cd4e3e0d4b5a52aad38aa49b8d6a0
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2023-08-02 22:20:59 +0100

    matrix generation p.2, see desc.
    - auto-flow was not a problem as default is row, an issue, moreover a confusion was between indexing local variables: switching kj positions solved the issue, use diff for more...

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ const [
 ];
 
 matrix_label:; 
-/* DEV_NOTE # UNISCALAR has to go hand in hand with UNIDIMENSIONAL, whilst MULTISCALAR hand in hand with MULTIDIMENSIONA */
+/* DEV_NOTE # UNISCALAR has to go hand in hand with UNIDIMENSIONAL, whilst MULTISCALAR hand in hand with MULTIDIMENSIONAL */
 [...new Array(/* UNISCALAR ||  */MULTISCALAR).fill(0)].forEach((_, j)=>{
 
   new Row({id: `${Row.namespace}${1+j}`, shadowHost: `#${SHADOWHOST}`, dimConfig: {scalar: MULTISCALAR/*  || MULTISCALAR */, dimension: /* UNIDIMENSIONAL ||  */MULTIDIMENSIONAL}}) && [...new Array(/* UNISCALAR || */ MULTISCALAR).fill(0)].forEach((_, k)=>{
     const eachRow = document.getElementById(SHADOWHOST).children;
     /* DEV_NOTE # shadow root property is not mandatory, although it indicates open shadow dom */
     eachRow[j].shadowRoot.appendChild(
-        new Col({id: `${Row.namespace}${1+j}:${Col.namespace}${1+k}`})
+        new Col({id: `${Row.namespace}${1+k}:${Col.namespace}${1+j}`})
       )
   });
 
